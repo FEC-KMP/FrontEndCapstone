311311a303d4892aea4107af08f2ab34a811a7de
Kirk Easton <kirkeast@gmail.com>
2021-06-07 11:40:09 -0600

    changed permissions for coveralls

--- spec/ratingsReviews.test.js
+++ spec/ratingsReviews.test.js
@@ -3,7 +3,7 @@
 // import productMainView from '../client/src/components/productMainView/productMain.jsx';
 // import questionsAnswersView from '../client/src/components/questionsAnswersView/questionsAnswers.jsx';
 // import ratingsReviewsView from '../client/src/components/ratingsReviewsView/ratingsReviews.jsx';
-
+//something different
 
 test('example test', () => {
   expect(1).toBe(1);
