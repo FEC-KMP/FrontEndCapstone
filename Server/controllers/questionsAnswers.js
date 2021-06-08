2a2e0c159814d96ba89bcfcff97dab61dc2443a0
Phong Trinh <thanhphongus@gmail.com>
2021-06-08 13:11:37 -0600

    routing 2 func

--- Server/controllers/questionsAnswers.js
+++ Server/controllers/questionsAnswers.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const router = express.Router();
 
-const { GITHUB_API_KEY } = require('../config.js');
+const {GITHUB_API_KEY} = require('../config');
 const BaseUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/qa/questions';
 
 
@@ -27,8 +27,10 @@ router.get('/questions', (req, res) => {
 });
 
 router.get('/questions/:question_id/answers', (req, res) => {
-  console.log(req.params);
-  axios.get(BaseUrl, { headers: { Authorization: GITHUB_API_KEY } }, req.params.question_id)
+  console.log(req.params.question_id);
+  const id = req.params.question_id;
+  const url = `${BaseUrl}/${id}/answers`;
+  axios.get(url, { headers: { Authorization: GITHUB_API_KEY } })
     .then((response) => {
       res.status(200).send(response.data);
     })
@@ -37,5 +39,5 @@ router.get('/questions/:question_id/answers', (req, res) => {
       res.status(404).send(err);
     });
 });
-//  router.get('/reviews', ())
+//  router.get('/reviews',
 module.exports = router;
