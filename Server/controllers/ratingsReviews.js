9d9fae76c55b2ec2ce3a99353110c5f1f00bd0f3
Kirk Easton <kirkeast@gmail.com>
2021-06-08 13:22:10 -0600

    Working on routing

--- Server/controllers/ratingsReviews.js
+++ Server/controllers/ratingsReviews.js
@@ -1,7 +1,43 @@
-var models = require('../models');
+const express = require('express');
+const axios = require('axios');
 
-module.exports = {
-// http requests for Product Main here
+const router = express.Router();
+
+const { GITHUB_API_KEY } = require('../config.js');
+const BaseUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/rnr/reviews';
 
 
-};
+router.get('/reviews', (req, res) => {
+  axios.get(BaseUrl, {
+    headers: { Authorization: GITHUB_API_KEY },
+    params: {
+      // eslint-disable-next-line camelcase
+      product_id: req.query.product_id,
+      page: req.query.page,
+      count: req.query.count,
+      sort: req.query.sort
+    }
+  })
+    .then((response) => {
+      // console.log('this is response inside get request',response);
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      console.log('err inside routes.js');
+      res.status(404).send(err);
+    });
+});
+
+router.get('/reviews/meta/:product_id', (req, res) => {
+  console.log(req.params);
+  axios.get(BaseUrl, { headers: { Authorization: GITHUB_API_KEY } }, req.params.product_id)
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((err) => {
+      console.log('err inside routes.js');
+      res.status(404).send(err);
+    });
+});
+//  router.get('/reviews', ())
+module.exports = router;
