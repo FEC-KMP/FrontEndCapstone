07b7b58b9bf9148e36333a802d26f088183c3603
mrprov12 <mrprov@mac.com>
2021-06-11 12:46:03 -0600

    Added bootstrap v 4 updated bootstrap react vs to 1.6.1 reset navbar

--- Client/src/components/productMainView/ImageGallery/ImageGallery.jsx
+++ Client/src/components/productMainView/ImageGallery/ImageGallery.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import CaroselImg from './CaroselImg.jsx';
+import CaroselThumbnails from './CaroselThumbnails.jsx';
 
 var ImageGallery = ({ currentStyleObj }) => {
   if (!currentStyleObj) { return 'data not found'; }
   return (
     <div className="ImageGallery">
+      <CaroselThumbnails />
       <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
         <ol className="carousel-indicators">
-          <li data-target="#className" data-slide-to="0" className="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          <li data-target="#carousel-thumb" data-slide-to="0" className="active"> <img className="d-block w-100" src="../../../../dist/imgs/pug1.jpeg"
+            className="img-fluid" /></li>
+          <li data-target="#carousel-thumb" data-slide-to="1"><img className="d-block w-100" src="../../../../dist/imgs/pug2.jpeg"
+            className="img-fluid" /></li>
+          <li data-target="#carousel-thumb" data-slide-to="2"><img className="d-block w-100" src="../../../../dist/imgs/pug3.jpeg"
+            className="img-fluid" /></li>
         </ol>
         <div className="carousel-inner">
           {(currentStyleObj.photos).map(((photoObj) => {
-            return <CaroselImg photoObj={photoObj} index={currentStyleObj.photos.indexOf(photoObj)}/>;
+            return <CaroselImg photoObj={photoObj} index={currentStyleObj.photos.indexOf(photoObj)} />;
           }))}
         </div>
         <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -25,6 +30,7 @@ var ImageGallery = ({ currentStyleObj }) => {
           <span className="sr-only">Next</span>
         </a>
       </div>
+
     </div>
   );
 };
