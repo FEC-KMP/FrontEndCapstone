e92dd47be71ad5455634a6a265a773429e7258f0
Phong Trinh <thanhphongus@gmail.com>
2021-06-15 08:40:15 -0600

    search for question form

--- Client/src/components/questionsAnswersView/QAList.jsx
+++ Client/src/components/questionsAnswersView/QAList.jsx
@@ -1,18 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import AnswerList from './AnswerList.jsx';
+import { qAContext } from '../../context/QAContext.jsx';
 
-const QAList = ({questionsList}) => {
-  return (
+const QAList = ({ question }) => {
+
+
+  return (question && question.answers) ? (
     <div>
       <div>
-        <h3>Q: Question</h3>
-        <h5>A: Answers</h5>
-        <h3>Q: Question</h3>
-        <h5>A: Answers</h5>
-        <h3>Q: Question</h3>
-        <h5>A: Answers</h5>
+        <div>
+          <h3>Q: {question.question_body}</h3>
+          <span> Helpful?</span>
+          <button type="button" className="btn btn-link">
+            <div>
+              Yes({question.question_helpfulness}) |
+            </div>
+          </button>
+          <button type="button" className="btn btn-link">Add Answer</button>
+        </div>
+        <h3>A:</h3>
+        <AnswerList
+          answerList={question.answers} />
       </div>
     </div>
-  );
+  ) : null;
 };
 
 export default QAList;
+
+{/* <div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-4">
+      <div id="summary">
+        <p class="collapse" id="collapseSummary">
+
+        </p>
+        <a class="collapsed" data-toggle="collapse" href="#collapseSummary" aria-expanded="false" aria-controls="collapseSummary"></a>
+      </div>
+      </div>
+      </div> */}
