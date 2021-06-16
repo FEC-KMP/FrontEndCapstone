11ef06388f7149e77c2ed77ca85d0f877dd4a478
mrprov12 <mrprov@mac.com>
2021-06-16 16:50:53 -0600

    Thumbnail selection fixed

--- Client/src/components/productMainView/StyleSelector/Thumbnail.jsx
+++ Client/src/components/productMainView/StyleSelector/Thumbnail.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-var Thumbnail = ({ styleObj, onStyleThumbnailClick }) => {
+var Thumbnail = ({ styleObj, onStyleThumbnailClick, selectedStyleName, updateSelectedStyleName }) => {
 
   return (
     <div>
-      <img className={`styleThumbnail style${styleObj.style_id}`} src={`${styleObj.photos[0].thumbnail_url}`} key={`${styleObj.style_id}`} onClick={() => { onStyleThumbnailClick(styleObj.style_id); }} />
+      <img className={`styleThumbnail style${styleObj.style_id} ${styleObj.name === selectedStyleName ? 'selectedStyle' : ''}`} src={`${styleObj.photos[0].thumbnail_url}`} key={`${styleObj.style_id}`} onClick={() => { onStyleThumbnailClick(styleObj.style_id); updateSelectedStyleName(styleObj.name); }} />
     </div>
   );
 };
